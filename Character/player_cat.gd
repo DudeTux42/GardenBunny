b328e83fdb38158c1bb86a6ee1eb9689bc26f4ef
DudeTux42 <lindermayr@pm.me>
2024-03-04 11:07:02 +0100

    set up in vscode

--- Character/player_cat.gd
+++ Character/player_cat.gd
@@ -2,12 +2,14 @@ extends CharacterBody2D
 
 @export var move_speed : float = 100
 
+parameters/Idle/blend_position
+
 func _physics_process(_delta):
 	var input_direction = Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
 		Input.get_action_strength("down") - Input.get_action_strength("up")
 	)
 	
-	velocity = input_direction * move_speed
+	velocity = input_direction.normalized() * move_speed
 	
 	move_and_slide()
